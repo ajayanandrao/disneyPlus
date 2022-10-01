ba195f8941bddbe345199372808d7a2a071e0101
Ajay Anandrao <32159029+ajayanandrao@users.noreply.github.com>
2022-10-01 19:35:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import { Provider } from 'react-redux';
 import './App.scss';
 import store from './redux/Store/Store';
-import { motion } from 'framer-motion';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <>
       <Provider store={store}>
-        <div className='container'>
-          <div className='c'>
-
-            <motion.h2 className='h2'
-              initial={{ y: -100, opacity: 0, }}
-              transition={{ delay: 1, duration: 1.5 }}
-              animate={{
-                opacity: 1, y: 0
-              }}>
-              Wellcome Ajay
-            </motion.h2>
-          
-          </div>
-        </div>
+        <Router basename='/diseny'>
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+          </Routes>
+        </Router>
       </Provider>
     </>
   );
